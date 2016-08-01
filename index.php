ca762db25a3b65f1411dac5ce5994f735c67de5a
Paolo Occhioni <occhionip@gmail.com>
2016-08-01 10:50:40 +0200

    Aggiornata la somma

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
   <title>Somma PHP</title>
  </head>
  <body>
- 
- <?php echo "Somma<p>"; ?>
+   <form action="index.php" method="POST">
+   <p>A: <input type="text" name="a" value="" /></p>
+   <p>B: <input type="text" name="b" value ="" /></p>
+   <p><input type="submit"></p>
+  </form>
+  <?php echo "Somma di " .$_POST['a']. " con " .$_POST['b']. ": " .($_POST['a'] + $_POST['b']). "<p>"; ?>
+
+
  </body>
 </html>

726198cdae104fd316234cf266c9d9c733645adf
Paolo Occhioni <occhionip@gmail.com>
2016-08-01 11:38:09 +0200

    Somma di 3 numeri (nuovo)

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
    <form action="index.php" method="POST">
    <p>A: <input type="text" name="a" value="" /></p>
    <p>B: <input type="text" name="b" value ="" /></p>
+   <p>C: <input type="text" name="c" value ="" /></p>
    <p><input type="submit"></p>
   </form>
-  <?php echo "Somma di " .$_POST['a']. " con " .$_POST['b']. ": " .($_POST['a'] + $_POST['b']). "<p>"; ?>
+  <?php echo "Somma di " .$_POST['a']. " con " .$_POST['b']. " e con " .$_POST['c']. ": " .($_POST['a'] + $_POST['b'] + $_POST['c']). "<p>"; ?>
 
 
  </body>
